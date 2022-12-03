f9f6204bb20428b1a275761081be2bc9d8298ca6
Rayane Bendahmane <rayanebe570@gmail.com>
2022-12-03 13:16:10 +0000

    Update fire.py implement list fire builds

--- game/model/fire.py
+++ game/model/fire.py
@@ -1,15 +1,10 @@
-import pygame as pg
-import random
-
-
 class Fire: 
     def __init__(self, world):
-        '''probability calculated every second to ignite a random tile'''
-        #self.fire = pg.transform.scale(self.fire, (self.world.width, self.world.height))
-        #self.fire = pg.transform.rotate(self.fire, random.randint(0, 360))
-        #self.fire = pg.transform.flip(self.fire, random.randint(0, 1), random.randint(0, 1))
         '''get world parameters'''
         self.world = world
+        '''list building in fire'''
+        self.fire = []
+
 
 
 
@@ -21,4 +16,9 @@ class Fire:
                 if self.world.world[x][y].get_tile() == 'hud_house_sprite':
                     if random.randint(0,100) < 50:
                         self.world.world[x][y].set_tile('fire')
-
+                        temp = {
+                            "image": self.world.world[x][y].get_tile(),
+                            "x": x,
+                            "y": y
+                        }
+                        self.fire.append(temp)
